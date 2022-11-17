3b9c0e9afdc4ec3c0387fb11faaad6b2a955597b
Yee Jin <yeejin0406@gmail.com>
2022-11-17 22:21:18 +0800

    working on ex 2.1

---
+++ courseinfo/src/components/Course.js
@@ -0,0 +1,27 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+const Course = ({ course }) => {
+
+}
+
+export default Course;
